274697ea7c97c7620a631fcf4fdb0c2443c429d7
Hasan201601 <hasanuzzaman201601@gmail.com>
2021-08-15 09:45:25 +0600

    withdrawal created and programmed

--- js/banking.js
+++ js/banking.js
@@ -16,13 +16,43 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
     //update account balance
     const balanceTotal = document.getElementById('balance-amount');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const previousBalanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalText);
 
     const newBalanceTotal = previousBalanceTotal + newDepositTotal;
     balanceTotal.innerText = newBalanceTotal;
 
     // clear the deposit input field 
     depositInput.value = '';
+
+})
+
+// handle withdraw button event
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    // get the withdrawal amount 
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawAmountText);
+
+    // get the total withdraw amount 
+    const withdrawTotal = document.getElementById('withdraw-amount');
+    const previousWithdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+
+    // pass the withdrawn amount to withdraw
+    const newWithdrawTotal = previousWithdrawTotal + withdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // update account balance
+    const balanceTotal = document.getElementById('balance-amount');
+    const previousBalanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalText);
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawTotal;
+    balanceTotal.innerText = newBalanceTotal;
+
+
+    // clear the withdraw input field 
+    withdrawInput.value = '';
 
 })
